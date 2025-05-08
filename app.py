602d8cf1160be206b99fecb923658a5c159453f3
27stephentran <121224237+27stephentran@users.noreply.github.com>
2025-05-08 10:35:54 +0000

    Update All

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 from flask import Flask, request, render_template
+from random import choice
+import database
+import os
 
+leaderboard = []
+attempts = 1
+secret_word = ""
+name = ''
+if not os.path.exists("valorant_wordle.db"):
+    database.make_database()
+WORDS = database.get_words()
+print(WORDS)
+
+def choose_word():
+    global WORDS
+    chosen_word = choice(WORDS) 
+    word_id = chosen_word[0]
+    secret_word = chosen_word[1].upper()
+    return word_id, secret_word
+
+word_id, secret_word = choose_word()
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,7 +28,40 @@ def index():
 
 @app.route("/wordle_game", methods=["GET", "POST"])
 def wordle_game():
-    return render_template("wordle_game.html")
+    global word_id, secret_word, leaderboard, attempts, name
+    print(secret_word)
+    guess = [""] * len(secret_word)
+    if request.method == "POST":
+        name = request.form.get("name_input").upper()
+        guess = [request.form.get(f"letter{i+1}", "").upper()
+                 for i in range(len(secret_word))]
+        guess_word = "".join(guess)
+
+        if len(guess_word) != len(secret_word):
+            return render_template("wordle_game.html", error="Not Enough Letters", word_length=len(secret_word), guess=guess, leaderboard = leaderboard, name = name)
+        
+        feedback = check_guess(guess_word, secret_word)
+
+        if guess_word == secret_word:
+            database.add_player(name, attempts, word_id)
+            leaderboard = database.get_leaderboard(word_id)
+            return render_template("wordle_game.html", success=True, feedback=feedback, word_length=len(secret_word), guess=guess, leaderboard = leaderboard, name = name)
+        
+        return render_template("wordle_game.html", feedback=feedback, word_length=len(secret_word), guess=guess, leaderboard = leaderboard, name = name)
+    return render_template("wordle_game.html", word_length=len(secret_word), guess=guess, leaderboard = leaderboard, name = name)
+
+def check_guess(guess_word, secret_word):
+    global attempts
+    attempts += 1
+    feedback = []
+    for i in range(len(guess_word)):
+        if guess_word[i] == secret_word[i]:
+            feedback.append("Correct!")
+        elif guess_word[i] in secret_word:
+            feedback.append("Wrong Position!")
+        else:
+            feedback.append("Incorrect!")
+    return feedback
 
 if __name__ == "__main__":
     app.run()
